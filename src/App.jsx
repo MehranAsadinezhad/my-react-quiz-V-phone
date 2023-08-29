0e0e7625f7abdd960f811de651de63a829635313
Mehran <mehranasadition@gmail.com>
2023-08-29 07:02:34 +0430

    json server is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import Home from "./features/Home";
-import Questions from "./features/Questions";
+import { useEffect } from 'react';
+import Home from './features/Home';
+import Questions from './features/Questions';
+
 function App() {
+  
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err.message));
+  }, []);
+
   return (
-    <div className="bg-darkest h-screen w-screen fixed">
+    <div className="bg-darkest fixed h-screen w-screen">
       <Home />
       {/* <Questions/> */}
     </div>
