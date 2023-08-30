fbb0d3f2a91e36e32dfe8cc7d5da1a560933e215
Mehran <mehranasadition@gmail.com>
2023-08-30 13:29:31 +0430

    delete fake api

--- src/ui/Option.jsx
+++ src/ui/Option.jsx
@@ -11,7 +11,7 @@ export default function Option({ answer, question, dispatch }) {
           onClick={() => dispatch({ type: 'newAnswer', payload: index })}
           className={`my-1 w-64 rounded-full 
     bg-dark text-start text-light transition-all hover:bg-darkest hover:ring-2 hover:ring-dark md:w-96 md:px-6 px-5 py-3
-    md:py-4 md:text-lg
+    md:py-4 md:text-lg 
     ${hasAnswered && 'cursor-not-allowed'}
     ${index === answer ? 'md:translate-x-6 lg:translate-x-8 translate-x-3' : ''}
     ${
